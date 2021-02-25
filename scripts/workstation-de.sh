9a09f7b58f9da406ed90ff7c79a8b22e3c8c9803
Ilian Ivanov <smartcrap024@gmail.com>
2021-02-25 20:50:16 +0000

    Adding a provisioning script for the desktop environment(workstation edition)

---
+++ scripts/workstation-de.sh
@@ -0,0 +1,11 @@
+# Workstation installation script for Fedora 33 server edition
+# This is needed because auto unattend installation with kickstarter file is not supported
+
+# Install the Workstation group
+sudo yum groupinstall "Fedora Workstation" --skip-broken -yum
+
+# Change systemd to start graphical mode
+sudo systemctl set-default graphical.target
+
+# Reboot system for changes to take effect
+sudo systemctl reboot
