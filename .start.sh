9d9eba7b1846fbed96ce2af50e4957707ac84771
Brian Zalewski <brian@megabyte.space>
2021-05-06 14:16:49 -0400

    Updates

--- .start.sh
+++ .start.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# This script performs maintenance on this repository. It ensures git submodules are
+# This script performs maintenance on Packer repositories. It ensures git submodules are
 # installed and then copies over base files from the modules. It also generates the
-# documentation.
+# documentation and performs other miscellaneous tasks.
 
-set -e
-REPO_TYPE=packer
+set -ex
+
+curl -sL https://git.io/_has | bash -s docker git jq node npm wget
+export REPO_TYPE=packer
 git submodule update --init --recursive
 if [ ! -f "./.modules/${REPO_TYPE}/update.sh" ]; then
   git submodule add -b master https://gitlab.com/megabyte-space/common/$REPO_TYPE.git ./.modules/$REPO_TYPE
 else
   cd ./.modules/$REPO_TYPE
-  git checkout master && git pull
+  git checkout master && git pull origin master
   cd ../..
 fi
 bash ./.modules/$REPO_TYPE/update.sh
